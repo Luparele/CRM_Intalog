3a6e2a69c3bd1a6f6bc8480d723f7aa5d06d0eca
Luparele <eduardo.luparele@gmail.com>
2026-05-08 10:02:49 -0300

    Chore: configure Google Drive folder ID for backups and update .gitignore

--- CRM_Comercial/settings.py
+++ CRM_Comercial/settings.py
@@ -251,5 +251,5 @@ WEBPUSH_SETTINGS = {
 
 # --- CONFIGURAÇÕES DE BACKUP (Google Drive) ---
 # Substitua pelo ID da pasta do Google Drive onde os backups serão salvos
-GOOGLE_DRIVE_BACKUP_FOLDER_ID = 'SUA_ID_DA_PASTA_AQUI'
+GOOGLE_DRIVE_BACKUP_FOLDER_ID = '1yMyVjyxdVE5s0AXwdlZvN5L5z5kxcqe_'
 
